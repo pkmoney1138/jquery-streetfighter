d75394b903f7dd1d21c01a79ce4a606acd603ff2
pkmoney1138 <pkmoney@comcast.net>
2015-02-23 13:37:25 -0500

    ryu animation and sound finished

--- js/app.js
+++ js/app.js
@@ -8,14 +8,25 @@ $(document).ready(function() {
 		$(".ryu-still").show();
 	})
 	.mousedown(function() {
-		// play hadouken sound
+		playHadouken();
 		$(".ryu-ready").hide();
 		$(".ryu-throwing").show();
-		$(".hadouken").show();
-		//animate it to the right of the screen
+		$(".hadouken").finish().show().animate(
+			{"left": "500px"},
+			1000,
+			function() {
+				$(this).hide();
+				$(this).css("left","-212px");
+			});
 	})
 	.mouseup(function() {
 		$(".ryu-throwing").hide();
 		$(".ryu-ready").show();
 	});
 });
+
+function playHadouken () {
+	$("#hadouken-sound")[0].volume = 0.5;
+	$("#hadouken-sound")[0].load();
+	$("#hadouken-sound")[0].play();
+}
